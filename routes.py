7c3a38fb27dc6fd8924fe687cb1e38ef926441c0
teejs <teejs2012@gmail.com>
2016-12-23 17:13:29 +0800

    added models and forms

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
+db.init_app(app)
+
+app.secret_key = "development-key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flask1'
 
 @app.route("/")
 def index():
@@ -9,6 +16,14 @@ def index():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
+	form = SignupForm()
+
+	if request.method == "POST":
+		return "Success"
+	elif request.method == "GET":
+		return render_template('signup.html', form = form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
